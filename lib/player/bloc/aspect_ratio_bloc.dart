e83c2c31c5b10e4958123af49c57c0e794442407
Hussnain Haidar <hussnaintarar@gmail.com>
2020-01-03 11:08:34 +0500

    aspect ratio name change done.

--- lib/player/bloc/aspect_ratio_bloc.dart
+++ lib/player/bloc/aspect_ratio_bloc.dart
@@ -7,6 +7,7 @@ import 'package:videos_sharing/player/bloc/state/aspect_ratio.dart';
 enum AspectRatioEvents { original, fourByThree, sixteenByNine }
 
 class AspectRatioBloc extends Bloc<AspectRatioState, AspectRatioState> {
+  Timer _timer;
   @override
   AspectRatioState get initialState => AspectRatioState(
       0.0, Icons.aspect_ratio, "Original", AspectRatioEvents.original, false);
@@ -15,5 +16,20 @@ class AspectRatioBloc extends Bloc<AspectRatioState, AspectRatioState> {
   Stream<AspectRatioState> mapEventToState(
       AspectRatioState aspectRatioState) async* {
     yield aspectRatioState;
+    _hideATimer();
+  }
+
+  _hideATimer() {
+    if (_timer != null && _timer.isActive) {
+      _timer.cancel();
+      _timer = Timer(Duration(seconds: 1), _hideA);
+    } else {
+      _timer = Timer(Duration(seconds: 1), _hideA);
+    }
+  }
+
+  _hideA() {
+    this.add(AspectRatioState(state.aspectRatio, state.icon,
+        state.aspectRatioName, state.aspectRatioEvents, false));
   }
 }
